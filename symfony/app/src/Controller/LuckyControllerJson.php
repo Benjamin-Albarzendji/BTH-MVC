63ff989b9c8e6d224bccc3cadd4c7c24405f5a38
Benjamin-Albarzendji <105798796+Benjamin-Albarzendji@users.noreply.github.com>
2024-05-16 00:04:17 +0200

    Kmom01 done

---
+++ symfony/app/src/Controller/LuckyControllerJson.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class LuckyControllerJson
+{
+
+    #[Route("/api")]
+    public function jsonNumber(): Response
+    {
+
+
+        $data = [
+            '/api' => "This is the API endpoint",
+            '/api/quote' => "Get your daily dosage of wisdom",
+        ];
+
+        // return new JsonResponse($data);
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/quote")]
+    public function jsonQuote(): Response
+    {
+        $number = random_int(0, 3);
+
+        $quotes = [
+            "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+            "The way to get started is to quit talking and begin doing. -Walt Disney",
+            "Your time is limited, so don't waste it living someone else's life. -Steve Jobs",
+            "If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
+        ];
+
+        // Current date
+        $date = date('Y-m-d H:i:s');
+
+        $data = [
+            'quote' => $quotes[$number],
+            'Time' => $date
+        ];
+
+        // return new JsonResponse($data);
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
